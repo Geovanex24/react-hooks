a6aaef8978ba284353f2b8f99f449f32e4a3437f
geovanex24 <geovanex24@hotmail.com>
2024-08-02 15:37:03 -0600

    Pruebas sobre useCounter - CustomHook

---
+++ tests/hooks/useCounter.test.js
@@ -0,0 +1,58 @@
+import { renderHook } from "@testing-library/react";
+import { useCounter } from "../../src/hooks/useCounter";
+import { act } from "react";
+
+describe("Pruebas en el useCounter", () => {
+  test("debe de retornar los valores por defecto", () => {
+    const { result } = renderHook(() => useCounter());
+    const { counter, decrement, increment, reset } = result.current;
+
+    expect(counter).toBe(10);
+    expect(counter).toEqual(expect.any(Number));
+    expect(decrement).toEqual(expect.any(Function));
+    expect(increment).toEqual(expect.any(Function));
+    expect(reset).toEqual(expect.any(Function));
+  });
+
+  test("debe de generar el counter con el valor de 100", () => {
+    const { result } = renderHook(() => useCounter(100));
+    const { counter } = result.current;
+    expect(counter).toBe(100);
+  });
+
+  test("debe de incrementar el contador", () => {
+    const { result } = renderHook(() => useCounter(100));
+    const { increment } = result.current;
+
+    act(() => {
+      increment();
+      increment(5);
+    });
+
+    expect(result.current.counter).toBe(106);
+  });
+
+  test("debe de decrementar el contador", () => {
+    const { result } = renderHook(() => useCounter(100));
+    const { decrement } = result.current;
+
+    act(() => {
+      decrement();
+      decrement(5);
+    });
+
+    expect(result.current.counter).toBe(94);
+  });
+
+  test("debe de resetear el contador", () => {
+    const { result } = renderHook(() => useCounter(100));
+    const { decrement, reset } = result.current;
+
+    act(() => {
+      decrement();
+      reset();
+    });
+
+    expect(result.current.counter).toBe(100);
+  });
+});
